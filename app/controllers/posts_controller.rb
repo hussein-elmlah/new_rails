5ad1b2329d0808fd1d24dca28bdd9a34efd4ab7a
hussein-elmlah <husseinelmlah11@gmail.com>
2024-05-04 00:10:01 +0300

    create Post model and migrate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,2 @@
-class PostsController < ActionController::Base
-    def index
-        # post = Post.create({title: "t1", content: "content1"}) 
-        # post = Post.find(1)
-        post = Post.all
-        puts "\n++++ #{post.inspect} ++++\n\n"
-    end
+class PostsController < ApplicationController
 end
