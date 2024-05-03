ae49389c46dbafd496d7f96b5f18b88bcd945fe4
hussein-elmlah <husseinelmlah11@gmail.com>
2024-05-04 00:13:41 +0300

    add PostController methods and Post routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,13 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
-
-  get "posts", to: "posts#index"
+  # Routes for Post resource
+  get '/posts', to: 'posts#index', as: 'posts'
+  get '/posts/:id', to: 'posts#show', as: 'post'
+  get '/posts/new', to: 'posts#new', as: 'new_post'
+  post '/posts', to: 'posts#create'
+  get '/posts/:id/edit', to: 'posts#edit', as: 'edit_post'
+  patch '/posts/:id', to: 'posts#update'
+  delete '/posts/:id', to: 'posts#destroy'
 
 end
